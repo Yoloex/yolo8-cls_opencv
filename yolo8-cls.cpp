fb6f1226824be6d07c3eab993f2b2aee0808dcd3
yoloex <jason.lanwin@gmail.com>
2024-01-10 22:49:56 +0900

    add func

--- yolo8-cls.cpp
+++ yolo8-cls.cpp
@@ -3,28 +3,30 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char *argv[])
-{
-	string model_path = argv[1], img_path = argv[2];
+void infer(string model_path, string img_path) {
 	int imgh = 224, imgw = 224;
 
-	Mat input = imread(img_path), dst, blob;
+	Mat input = imread(img_path), dst;
 	vector<double> results;
 
 	dnn::Net model = dnn::readNetFromONNX(model_path);
 
 	resize(input, dst, Size(imgw, imgh), 0, 0, INTER_LINEAR);
-	dnn::blobFromImage(dst, blob, 1 / 255.0f, Size(dst.cols, dst.rows), Scalar(0, 0, 0), true, false);
+	dnn::blobFromImage(dst, input, 1 / 255.0f, Size(dst.cols, dst.rows), Scalar(0, 0, 0), true, false);
 
-	model.setInput(blob);
-	
-	vector<string> out_layer_names = model.getUnconnectedOutLayersNames();
+	model.setPreferableBackend(dnn::DNN_BACKEND_CUDA);
+	model.setPreferableTarget(dnn::DNN_TARGET_CUDA);
+	model.setInput(input);
+	results = model.forward();
 
-	model.forward(results, out_layer_names);
+	if (results[0] > results[1]) cout << "off" << endl;
+	else cout << "on" << endl;
+}
 
-	for (auto& result : results) {
-		cout << result << endl;
-	}
-	
+int main(int argc, char *argv[])
+{
+	string model_path = argv[1], img_path = argv[2];
+	infer(model_path, img_path);
+
 	return 0;
 }
